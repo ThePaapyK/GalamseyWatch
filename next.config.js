1713553885be21fbea8ff706142e2a831083c300
jyanokye <jamesyawanokye17@gmail.com>
2025-10-05 22:51:29 +0000

    Deploy

--- next.config.js
+++ next.config.js
@@ -4,15 +4,34 @@ const nextConfig = {
     appDir: true,
   },
   transpilePackages: ['kepler.gl'],
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    // Fix alias for Kepler.gl
     config.resolve.alias = {
       ...config.resolve.alias,
       'kepler.gl/components': 'kepler.gl/dist/components',
       'kepler.gl/actions': 'kepler.gl/dist/actions',
-      'kepler.gl/reducers': 'kepler.gl/dist/reducers'
-    }
-    return config
-  }
-}
+      'kepler.gl/reducers': 'kepler.gl/dist/reducers',
+    };
 
-module.exports = nextConfig
+    // Allow WebAssembly files to be properly handled
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    // Copy parquet_wasm_bg.wasm to the .next directory during build
+    config.module.rules.push({
+      test: /parquet_wasm_bg\.wasm$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/chunks/[name][ext]',
+      },
+    });
+
+    // Fix: ensure wasm files are loaded correctly at runtime
+    if (!isServer) {
+      config.resolve.fallback = { fs: false };
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
